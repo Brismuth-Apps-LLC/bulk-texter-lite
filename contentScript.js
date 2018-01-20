ae227e9929ee3734a3b541928efeacfdb4db4b93
brismuth <brismuth@gmail.com>
2018-01-20 02:24:03 -0700

    Conversation autoselection working. Speeding up UI.

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,55 @@
+var phoneNumber = '(801) 420-8328';
+var message = 'This is my really long message';
+
 // load script.js in iframes
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('script.js');
 (document.head||document.documentElement).appendChild(s);
 
+console.log("Loading content script");
 window.addEventListener("message", function(event) {
 	// We only accept messages from ourselves
 	if (event.source != window)
 		return;
 
-	if (event.data.type && (event.data.type == "NUMBER_INPUT_FILLED")) {
-		console.log("NUMBER_INPUT_FILLED, sending message to press space");
-		chrome.runtime.sendMessage({ action: 'pressSpace' });
+	if (event.data.type && (event.data.type === "NUMBER_INPUT_READY")) {
+		console.log("NUMBER_INPUT_READY, filling in number");
+
+		var numInput = document.querySelector('div[googlevoice="nolinks"] input[placeholder="Enter name, email, or phone"]');
+		numInput.value = phoneNumber;
+		numInput.focus();
+		numInput.select();
+		document.execCommand('cut');
+		document.execCommand('paste');
+
+		startChat();
+	}
+
+	if (event.data.type && (event.data.type === "MESSAGE_INPUT_READY")) {
+		console.log("MESSAGE_INPUT_READY, filling in message");
+
+		var messageEditor = document.querySelector('div.editable[g_editable="true"][role="textbox"][contenteditable="true"]');
+		messageEditor.innerText = message;
+		messageEditor.focus();
+		document.execCommand('selectAll', false, null);
+
+		// cut and paste
+		// document.execCommand('cut');
+		// messageEditor.focus();
+		// document.execCommand('paste');
 	}
 });
+
+// clicks the "start SMS" button on the number dropdown
+function startChat() {
+	var attempts = 20;
+	var keepTrying = setInterval(function() {
+		var startChatButton = document.querySelector('div[googlevoice="nolinks"] a[title="Click to send SMS"]');
+		if (startChatButton) {
+			startChatButton.click();
+		}
+		if (startChatButton || attempts-- < 0) {
+			clearInterval(keepTrying);
+		}
+	}, 70);
+}
--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-var phoneNumber = '###-###-####';
-var message = 'This is my really long message';
-// todo: replace with event
-setTimeout(function() {
-	findGoogleVoice(phoneNumber, message);
-}, 2000);
+(function keepTryingToFindGoogleVoice() {
+	var attempts = 25;
+	var keepTrying = setInterval(function() {
+		if (findGoogleVoice() || attempts-- < 0 || !window.location.href.includes('/webchat/')) {
+			clearInterval(keepTrying);
+		}
+	}, 200);
+})();
 
-function findGoogleVoice(phoneNumber, message) {
+function findGoogleVoice() {
+	var foundGoogleVoice = false;
+
+	// check if this is the conversation list
 	var button = document.querySelector('div[googlevoice="nolinks"] button');
 	if (button) {
 		console.log('Bulk SMS - found hangouts conversation list');
+
+		// this button adds the number input to the dom
 		button.click();
-		var numInput = document.querySelector('div[googlevoice="nolinks"] input[placeholder="Enter name, email, or phone"]');
-		numInput.value = phoneNumber;
-		numInput.focus();
 
-		// this should trigger a key event but the key event press isn't registering. Some work required.
-		var data = { type: "NUMBER_INPUT_FILLED" };
+		// tells the content script to paste in the next number
+		var data = { type: "NUMBER_INPUT_READY" };
 		window.postMessage(data, "*");
-
-		numInput.addEventListener("keyup", startChat, false);
+		foundGoogleVoice = true;
 	}
 
+	// check if this is the message editor
 	var messageEditor = document.querySelector('div.editable[g_editable="true"][role="textbox"][contenteditable="true"]');
 	if (messageEditor) {
 		console.log('Bulk SMS - found hangouts message thread');
-		messageEditor.innerText = message;
-	}
-}
 
-function startChat() {
-	setTimeout(function() {
-		var startChatButton = document.querySelector('div[googlevoice="nolinks"] a[title="Click to send SMS"]');
-		startChatButton.click();
-	}, 100);
+		// tells the content script to paste in the message
+		var data = { type: "MESSAGE_INPUT_READY" };
+		window.postMessage(data, "*");
+		foundGoogleVoice = true;
+	}
+
+	return foundGoogleVoice;
 }
