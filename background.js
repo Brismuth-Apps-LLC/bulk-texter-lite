5e72e8f9768858d8bc95a656f1d265349c88251f
brismuth <brismuth@gmail.com>
2018-01-31 23:28:43 -0700

    Fixing behavior when tab changes

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 // this acts as a proxy for the content scripts in the different iframes to talk to each other
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-		chrome.tabs.sendMessage(tabs[0].id, message, function(response) {});
-	});
+	chrome.tabs.sendMessage(sender.tab.id, message, function(response) {});
 });
