a382697038e5d9deab0fab481a68df9d77b0780c
Brian Smith <brismuth@gmail.com>
2023-05-25 13:06:47 -0600

    Resolves startChat failed issue

--- src/contentScripts/main.js
+++ src/contentScripts/main.js
@@ -7,7 +7,7 @@ const selectors = {
 	gvMessagesTab: 'a[gv-test-id="sidenav-messages"]',
 	gvNumInputButton: 'div[gv-id="send-new-message"]',
 	gvNumInput: 'gv-recipient-picker input[ng-show="ctrl.allowToAddRecipients()"], gv-recipient-picker input[placeholder="Type a name or phone number"]',
-	gvStartChatButton: 'gv-contact-list div[ng-class="::ctrl.CSS.SEND_TO_PHONE_NUMBER"]',
+	gvStartChatButton: 'gv-contact-list-ng2 .send-to-button, gv-contact-list div[ng-class="::ctrl.CSS.SEND_TO_PHONE_NUMBER"]',
 	gvRecipientButton: 'gmat-input-chip[gv-id="chip-phone-number"], div[aria-label="Select recipients"] md-chips md-chip button',
 	gvMessageEditor: 'textarea[gv-test-id="gv-message-input"], textarea[aria-label="Type a message"], textarea[aria-label="Add a caption"], #gv-message-input, div[gv-test-id="gv-message-input"]',
 	gvSendButtonOld: 'gv-icon-button[icon-name="send"] button[aria-label="Send message"]',
