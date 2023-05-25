b6c68a5d769f04c172b65d4a294666ce0d866011
Brian Smith <brismuth@gmail.com>
2023-05-25 13:06:32 -0600

    Adding international language support

--- src/contentScripts/main.js
+++ src/contentScripts/main.js
@@ -9,9 +9,9 @@ const selectors = {
 	gvNumInput: 'gv-recipient-picker input[ng-show="ctrl.allowToAddRecipients()"], gv-recipient-picker input[placeholder="Type a name or phone number"]',
 	gvStartChatButton: 'gv-contact-list div[ng-class="::ctrl.CSS.SEND_TO_PHONE_NUMBER"]',
 	gvRecipientButton: 'gmat-input-chip[gv-id="chip-phone-number"], div[aria-label="Select recipients"] md-chips md-chip button',
-	gvMessageEditor: 'textarea[aria-label="Type a message"], #gv-message-input, div[gv-test-id="gv-message-input"]',
+	gvMessageEditor: 'textarea[gv-test-id="gv-message-input"], textarea[aria-label="Type a message"], textarea[aria-label="Add a caption"], #gv-message-input, div[gv-test-id="gv-message-input"]',
 	gvSendButtonOld: 'gv-icon-button[icon-name="send"] button[aria-label="Send message"]',
-	gvSendButtonNew: 'gv-icon-button-ng2[icon-name="send"][label="Send message"] button',
+	gvSendButtonNew: 'gv-icon-button-ng2[gv-test-id="send_message"] button, gv-icon-button-ng2[icon-name="send"][label="Send message"] button',
 	gvSendingNote: 'gv-message-item div[ng-if="ctrl.shouldDisplayTransmissionStatus()"] div[ng-if="!ctrl.isFailed()"]',
 	gvMostRecentMessages: 'div[gv-id="content"] div[gv-test-id="bubble"] gv-annotation, gv-text-message-item gv-annotation',
 	gvChatLoadedHeader: 'gv-message-list-header p[gv-test-id="conversation-title"]'
